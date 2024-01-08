cbdb3750b1d8c2656d6ddf28e5acec0c217b3047
Katrina Guyduy <katrinaguyduy@gmail.com>
2024-01-07 22:52:13 -0800

    add condition if no match show empty message

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const searchInput = document.querySelector('#search');
 const divs = document.querySelectorAll('.recipe');
+const emptyMessage = document.querySelector('.empty');
 
 searchInput.addEventListener('keyup', (event) => {
     const word = event.target.value.toLowerCase();
+
+    let anyMatch = false;
+
     divs.forEach(item => {
-        item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none');
+        const recipeText = item.querySelector('p').textContent.toLowerCase();
+        if (recipeText.includes(word)) {
+            item.style.display = 'block';
+            anyMatch = true;
+        } else {
+            item.style.display = 'none';
+        }
     });
+
+    if (!anyMatch && word.length > 0) {
+        emptyMessage.style.display = 'block';
+    } else {
+        emptyMessage.style.display = 'none';
+    }
 });
 
 
